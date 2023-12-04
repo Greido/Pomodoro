118cc48002af41db8f16780d460284e88e2b2d35
Juan Corzo Barrera <juanpablocorzobarrera@gmail.com>
2023-12-04 13:54:15 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Platform ,Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+import {Audio} from "expo-av"
+const colors = ["#e8b5f7","#d191f7","#9356a0"]
+
 
 export default function App() {
+
+  //Variable para largar el boton
+  const [isRunning, setIsRunnig]= useState(false);
+
+  //Variable para mantener el tiempo que llevamos
+  const [time, setTime]= useState(25 * 60);
+
+  //Variable para saber en que tiempo estamos o en que tab estamos
+  const [currentTime, setCurrentTime]= useState("POMO"|"SHORT"|"BREAK");
+
+  //Variable para el boton dependiendo si esta activo o no
+  const [isActive,setIsActive]= useState(false);
+
+
+  //Agregamos la funcionalidad principal del timer
+
+  useEffect(()=>{
+    let interval = null;
+
+
+    if(isActive){
+      //Run timer
+      interval = setInterval(()=>{
+        setTime(time-1)
+      },1000)
+    }else{
+      //Clear interval
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsRunnig(prev => !prev)
+      setTime(isRunning ? 300 : 1500);
+    }
+
+    return()=>clearInterval(interval)
+  },[isActive, time])
+
+
+
+  //Funcion para manejar el cambio del boton para activar el timer
+
+  function handleStartStop(){
+    playSound()
+    setIsActive(!isActive);
+  }
+
+  async function playSound (){
+    const {sound} = await Audio.Sound.createAsync(
+      require("./assets/Click.mp3")
+    )
+    await sound.playAsync()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container,{backgroundColor:colors[currentTime]}]}>
+    <View style={{flex:1, paddingHorizontal:15, paddingTop: Platform.OS === 'android' && 30}}>
+      <Text
+      style={styles.text}>
+        Pomodoro
+      </Text>      
+
+        {/* Importacion del Header */}
+        <Header 
+        currentTime={currentTime}
+        setCurrentTime={setCurrentTime}
+        setTime={setTime}
+         />
+
+
+        {/* Importacion del timer */}
+
+        <Timer
+        time={time}
+        />
+
+        {/* Creamos el boton */}
+
+        <TouchableOpacity style={styles.boton} onPress={handleStartStop}>
+          <Text
+          style={{color:"white",fontWeight:"bold"}}
+          >
+            {isActive ? "STOP" :"START"}
+          </Text>
+        </TouchableOpacity>
+
+
+      </View>
+      </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
   },
+  text:{
+    fontSize:32,
+    fontWeight:'bold'
+  },
+  
+  boton:{
+    alignItems:'center',
+    backgroundColor:"#333333",
+    padding:15,
+    marginTop:15,
+    borderRadius:15
+  }
 });
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+
+const options = ["Pomodoro","Short Break","Long Break"];
+
+const Header = ({currentTime, setCurrentTime, setTime}) => {
+
+    function handlePress(index) {
+        const newTime = index === 0 ? 25 : index  === 1 ? 5 : 15;
+        setCurrentTime(index);
+        setTime(newTime * 60)
+    }
+
+  return (
+    <View style={{flexDirection:'row'}}>
+        {options.map((item, index)=>(
+            <TouchableOpacity 
+            key={index} 
+            onPress={()=>handlePress(index)} 
+            style={[styles.itemStyle,currentTime != index && {borderColor:"transparent"}]}
+            >
+                
+            <Text
+                style={{fontWeight:'bold'}}
+            >
+                {item}
+            </Text>
+            
+            </TouchableOpacity>
+        ))}
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    itemStyle:{
+        width:"33%",
+        alignItems:'center',
+        borderWidth:3,
+        padding:5,
+        borderColor:"white",
+        borderRadius:10,
+        marginVertical:20
+    }
+})
+
+export default Header
